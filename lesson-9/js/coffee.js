3ee006501154ac634b9e31d265cbbb429210da4e
Claire <cameron.liam.yon@gmail.com>
2026-07-15 20:05:13 -0400

    finished lesson 9

--- lesson-9/js/coffee.js
+++ lesson-9/js/coffee.js
@@ -1,15 +1,73 @@
 const output = document.querySelector('#output');
 
 /* STEP 1: Instead of a constructor function, let's try a JavaScript class called 'Coffee' */
+class Coffee {
+    size;
+    isDecaf;
 
+    constructor(size, isDecaf) {
+        this.size = size;
+        this.isDecaf = isDecaf;
+    }
+
+    serveIt() {
+        let cup = document.createElement("img");
+
+        cup.setAttribute("src", "images/coffee-cup.svg");
+
+        if(this.isDecaf == true) {
+            cup.setAttribute("src", "images/coffee-cup-green.svg");
+        }
+        else {
+            cup.setAttribute("src", "images/coffee-cup-purple.svg")
+        }
+
+        switch (this.size) {
+            case "small":
+                cup.setAttribute("height", 100);
+                cup.setAttribute("width", 100);
+                break;
+            case "medium":
+                cup.setAttribute("height", 150);
+                cup.setAttribute("width", 150);
+                break;
+            case "large":
+                cup.setAttribute("height", 200);
+                cup.setAttribute("width", 200);
+                break;
+            default:
+                cup.setAttribute("height", 150);
+                cup.setAttribute("width", 150);
+                break;
+        }
+
+        cup.setAttribute("title", `A ${this.size} ${this.isDecaf ? "decafinated" : "caffinated"} coffee.`);
+
+        output.appendChild(cup);
+    }
+}
 /* STEP 2: Instatiate a coffee based on the above constructor function */
+let claireCoffee = new Coffee("medium", false);
+claireCoffee.serveIt();
+
+let joeCoffee = new Coffee("large", true);
+joeCoffee.serveIt();
+
+let nedCoffee = new Coffee("small", false);
+nedCoffee.serveIt();
 
 /* STEP 3: Add a method to the Coffee class called serveIt() */
 
 /* STEP 4: Call up the serveIt() method */
 
 /* STEP 5: Define a subclass of the Coffee class */
-
+class Latte extends Coffee {
+    milkType;
+    constructor(size, isDecaf, milkType) {
+        super(size, isDecaf);
+        this.milkType = milkType;
+    }
+}
 /* STEP 6: Create a new instance of the Latte object */
 
 /* STEP 7: Call up the latteDesc() method for the above created Latte instance */
